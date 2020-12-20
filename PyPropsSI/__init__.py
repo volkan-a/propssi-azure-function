e3df1e26032783c8f768306e80c23f35e6baa17f
volkan.a <vakkaya@gmail.com>
2020-12-20 07:38:45 +0300

    ok

--- PyPropsSI/__init__.py
+++ PyPropsSI/__init__.py
@@ -1,7 +1,5 @@
-import logging
 from CoolProp.CoolProp import PropsSI
 import azure.functions as func
-import json
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -18,27 +16,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         f = req_body.get('f')
 
     if p and p1 and v1 and p2 and v2 and f:
-        res = {
-            "MolarMass": PropsSI("MOLARMASS", p1, v1, p2, v2, f),
-            "Temperature": PropsSI("T", p1, v1, p2, v2, f),
-            "Pressure": PropsSI("P", p1, v1, p2, v2, f),
-            "SpecificMassDensity": 1.0/PropsSI("DMASS", p1, v1, p2, v2, f),
-            "SpecificMolarDensity": 1.0/PropsSI("DMOLAR", p1, v1, p2, v2, f),
-            "SpecificMassInternalEnergy": PropsSI("UMASS", p1, v1, p2, v2, f),
-            "SpecificMolarInternalEnergy": PropsSI("UMOLAR", p1, v1, p2, v2, f),
-            "SpecificMassEnthalpy": PropsSI("HMASS", p1, v1, p2, v2, f),
-            "SpecificMolarEnthalpy": PropsSI("HMOLAR", p1, v1, p2, v2, f),
-            "SpecificMassEntropy": PropsSI("SMASS", p1, v1, p2, v2, f),
-            "SpecificMolarEntropy": PropsSI("SMOLAR", p1, v1, p2, v2, f),
-            "ThermalConductivity": PropsSI("CONDUCTIVITY", p1, v1, p2, v2, f),
-            "MassConstantPressureSpecificHeat": PropsSI("CPMASS", p1, v1, p2, v2, f),
-            "MolarConstantPressureSpecificHeat": PropsSI("CPMOLAR", p1, v1, p2, v2, f),
-            "MassConstantVolumeSpecificHeat": PropsSI("CVMASS", p1, v1, p2, v2, f),
-            "MolarConstantVolumeSpecificHeat": PropsSI("CVMOLAR", p1, v1, p2, v2, f),
-            "PrandtlNumber": PropsSI("PRANDTL", p1, v1, p2, v2, f),
-            "Viscosity": PropsSI("VISCOSITY", p1, v1, p2, v2, f),
-        }
-        return func.HttpResponse(json.dumps(res))
+        return func.HttpResponse(f"{PropsSI(p, p1, v1, p2, v2, f)}")
     else:
         return func.HttpResponse(
             "Please pass a valid input on the query string or in the request body",
--- PyPropsSIAll/__init__.py
+++ PyPropsSIAll/__init__.py
@@ -1,6 +1,6 @@
-import logging
 from CoolProp.CoolProp import PropsSI
 import azure.functions as func
+import json
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
@@ -9,15 +9,34 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     except ValueError:
         pass
     else:
-        p = req_body.get('p')
         p1 = req_body.get('p1')
         v1 = req_body.get('v1')
         p2 = req_body.get('p2')
         v2 = req_body.get('v2')
         f = req_body.get('f')
 
-    if p and p1 and v1 and p2 and v2 and f:
-        return func.HttpResponse(f"{PropsSI(p, p1, v1, p2, v2, f)}")
+    if p1 and v1 and p2 and v2 and f:
+        res = {
+            "MolarMass": PropsSI("MOLARMASS", p1, v1, p2, v2, f),
+            "Temperature": PropsSI("T", p1, v1, p2, v2, f),
+            "Pressure": PropsSI("P", p1, v1, p2, v2, f),
+            "SpecificMassDensity": 1.0/PropsSI("DMASS", p1, v1, p2, v2, f),
+            "SpecificMolarDensity": 1.0/PropsSI("DMOLAR", p1, v1, p2, v2, f),
+            "SpecificMassInternalEnergy": PropsSI("UMASS", p1, v1, p2, v2, f),
+            "SpecificMolarInternalEnergy": PropsSI("UMOLAR", p1, v1, p2, v2, f),
+            "SpecificMassEnthalpy": PropsSI("HMASS", p1, v1, p2, v2, f),
+            "SpecificMolarEnthalpy": PropsSI("HMOLAR", p1, v1, p2, v2, f),
+            "SpecificMassEntropy": PropsSI("SMASS", p1, v1, p2, v2, f),
+            "SpecificMolarEntropy": PropsSI("SMOLAR", p1, v1, p2, v2, f),
+            "ThermalConductivity": PropsSI("CONDUCTIVITY", p1, v1, p2, v2, f),
+            "MassConstantPressureSpecificHeat": PropsSI("CPMASS", p1, v1, p2, v2, f),
+            "MolarConstantPressureSpecificHeat": PropsSI("CPMOLAR", p1, v1, p2, v2, f),
+            "MassConstantVolumeSpecificHeat": PropsSI("CVMASS", p1, v1, p2, v2, f),
+            "MolarConstantVolumeSpecificHeat": PropsSI("CVMOLAR", p1, v1, p2, v2, f),
+            "PrandtlNumber": PropsSI("PRANDTL", p1, v1, p2, v2, f),
+            "Viscosity": PropsSI("VISCOSITY", p1, v1, p2, v2, f),
+        }
+        return func.HttpResponse(json.dumps(res))
     else:
         return func.HttpResponse(
             "Please pass a valid input on the query string or in the request body",
